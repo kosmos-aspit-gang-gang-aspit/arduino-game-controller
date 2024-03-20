f5f08f290d8d1fd9b4b617bddf8118f6a058999f
GARZTHONGK <albert.bruun.thomsen@gmail.com>
2024-03-20 14:32:58 +0100

    .

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,26 +1,24 @@
-// #include <Streaming.h>
-
-
-String pressedButtons = "";
-// int buttonState = 0;
-
+daString pressedButtons = "";
+String lastPressedButtons = "";
 void setup() {
-  // put your setup code here, to run once:
   pinMode(2, INPUT_PULLUP);
-  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   if (digitalRead(2) != LOW){
     pressedButtons += "w";
-    // Serial.println("W");
-    // delay(100);
   }
   if (digitalRead(3) != LOW){
     pressedButtons += "s";
   }
+  if (digitalRead(4) != LOW){
+    pressedButtons += "d";
+  }
+  if (digitalRead(5) != LOW){
+    pressedButtons += "a";
+  }
   Serial.println(pressedButtons);
+  lastPressedButtons = pressedButtons;
   pressedButtons = "";
-  // delay(100);
 }
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ Arduino = serial.Serial("com3", 9600)
 pressedButtons = []
 
 
-def controller(keystrokes):
+def controller(data):
     global pressedButtons
+
     if "w" in data and "w" not in pressedButtons:
         keyboard.press("w")
-        pressedButtons.append("w")
+        pressedButtons.insert(0, "w")
     elif "w" not in data and "w" in pressedButtons:
         keyboard.release("w")
         try:
@@ -20,19 +21,41 @@ def controller(keystrokes):
 
     if "s" in data and "s" not in pressedButtons:
         keyboard.press("s")
-        pressedButtons.append("s")
+        pressedButtons.insert(0, "s")
     elif "s" not in data and "s" in pressedButtons:
         keyboard.release("s")
         try:
             pressedButtons.remove("s")
         except ValueError:
             pass
+
+    if "a" in data and "a" not in pressedButtons:
+        keyboard.press("a")
+        pressedButtons.insert(0, "a")
+    elif "a" not in data and "a" in pressedButtons:
+        keyboard.release("a")
+        try:
+            pressedButtons.remove("a")
+        except ValueError:
+            pass
+
+    if "d" in data and "d" not in pressedButtons:
+        keyboard.press("d")
+        pressedButtons.insert(0, "d")
+    elif "d" not in data and "d" in pressedButtons:
+        keyboard.release("d")
+        try:
+            pressedButtons.remove("d")
+        except ValueError:
+            pass
+
     print("pressed buttons: ", pressedButtons)
 
+# wsadwsadaaaaaaaaaaaaaaaaaaaawssddaaawsadaaaaaaaaaaaddwsawsdawsdaswdawsaddaaaaaaddaadaadaddaaddaaaddaaaadddadaadadadadawsadsadwwawwwadaadawsw
 
 while True:
     data = str(Arduino.readline().decode("ascii")).lower()
     # data = "w"
     print("data: ", data)
     controller(data)
-w
+
